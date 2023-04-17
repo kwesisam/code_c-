dea17c6eca723454acb836d96f466ff551becd8f
skm <kwesisam270@gmail.com>
2023-04-17 00:43:03 +0000

    c++-program

---
+++ programmingAndProblemAnalysis/dataTypes.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	// types of data types
+	/*
+	 * int (integer)
+	 * float (float point)
+	 * double 
+	 * bool (boolean)
+	 * char 
+	 * string
+	 *
+	 */		
+}
---
+++ programmingAndProblemAnalysis/variableDeclaration.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//dataType variableName;
+	/*
+	 *int age;
+	 * float cgpa;
+	 *char grade;
+	 *string name;
+	 
+	 */
+
+	//variable initiailization in c++
+	// static initialization
+	/*
+	 * int age;
+	 * age = 20;
+	 *
+	 * float height;
+	 * height = 6.2;
+	 *
+	 * int age = 20;
+	 * float height = 6.2;
+	 *
+	 * float height, weight;
+	 * height = 6.2;
+	 * weight = 70;
+	 *
+	 * float height, weigth;
+	 * height, weight = 70;
+	 *
+	 * int age(20);
+	 * float height(6.2);
+	 *
+	 * auto age(20);
+	 * auto height(6.2);
+	 *
+	 */
+
+	//dynamic initiailzation
+	/*
+	 * float height;
+	 * double weight;
+	 *
+	 * cout << "What is your height and weight?" << endl;
+	 * cin >> height >> weight;
+	 *
+	 */
+
+
+
+}
